45580c0dedd336987c64c5d83413ff3dee588d10
cheona <ehgus97334@gmail.com>
2023-08-26 03:56:14 +0900

    백엔드 기초세팅

---
+++ back/smudiary/src/main/java/com/webjin/smudiary/SmudiaryApplication.java
@@ -0,0 +1,13 @@
+package com.webjin.smudiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmudiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmudiaryApplication.class, args);
+	}
+
+}
