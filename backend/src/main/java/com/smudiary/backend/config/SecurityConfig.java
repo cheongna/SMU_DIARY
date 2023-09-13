fda6911c1937073901868d86262a5fd9abf13cc1
cheona <ehgus97334@gmail.com>
2023-09-14 06:23:46 +0900

    스프링 시큐리티 추가

---
+++ backend/src/main/java/com/smudiary/backend/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.smudiary.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(auth -> {
+                    auth.anyRequest().permitAll();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
