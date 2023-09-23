fbd735dfb5e0091f532ce22b9a94cce469c84b5e
mk <gwan5747@gmail.com>
2023-09-23 23:30:54 +0900

    프로필 페이지의 카테고리(홈, 프로필, 다이어리, 사진첩, 관리) 클릭시 스타일 변경되도록 이벤트 연결로 구현
    + 코드가 너무 지저분해서 나중에 좋은 코드로 변경할 예정

---
+++ front/seomyeonggwan/profile_part/profile_20questions.js
@@ -0,0 +1,174 @@
+window.onload = function() {
+    let home_btn = document.getElementById('home_btn');
+    let profile_btn = document.getElementById('profile_btn');
+    let diary_btn = document.getElementById('diary_btn');
+    let gallery_btn = document.getElementById('gallery_btn');
+    let adm_btn = document.getElementById('adm_btn');
+
+
+    home_btn.onclick = function() {
+        profile_btn.style.color = 'white';
+        profile_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        profile_btn.style.borderRadius = '5px';
+        profile_btn.style.borderWidth = '0px';
+        profile_btn.style.right = '0.15vw';
+
+        diary_btn.style.color = 'white';
+        diary_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        diary_btn.style.borderRadius = '5px';
+        diary_btn.style.borderWidth = '0px';
+        diary_btn.style.right = '0.15vw';
+
+        gallery_btn.style.color = 'white';
+        gallery_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        gallery_btn.style.borderRadius = '5px';
+        gallery_btn.style.borderWidth = '0px';
+        gallery_btn.style.right = '0.15vw';
+
+        adm_btn.style.color = 'white';
+        adm_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        adm_btn.style.borderRadius = '5px';
+        adm_btn.style.borderWidth = '0px';
+        adm_btn.style.right = '0.15vw';
+
+        home_btn.style.color = 'black';
+        home_btn.style.backgroundColor = 'white';
+        home_btn.style.borderRadius = '3px 8px 8px 3px';
+        home_btn.style.border = '1px solid rgb(132, 192, 233)';
+        home_btn.style.borderLeftWidth = '0px';
+        home_btn.style.right = "0.25vw";
+    }
+
+    profile_btn.onclick = function() {
+        home_btn.style.color = 'white';
+        home_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        home_btn.style.borderRadius = '5px';
+        home_btn.style.borderWidth = '0px';
+        home_btn.style.right = '0.15vw';
+
+        diary_btn.style.color = 'white';
+        diary_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        diary_btn.style.borderRadius = '5px';
+        diary_btn.style.borderWidth = '0px';
+        diary_btn.style.right = '0.15vw';
+
+        gallery_btn.style.color = 'white';
+        gallery_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        gallery_btn.style.borderRadius = '5px';
+        gallery_btn.style.borderWidth = '0px';
+        gallery_btn.style.right = '0.15vw';
+
+        adm_btn.style.color = 'white';
+        adm_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        adm_btn.style.borderRadius = '5px';
+        adm_btn.style.borderWidth = '0px';
+        adm_btn.style.right = '0.15vw';
+
+        profile_btn.style.color = 'black';
+        profile_btn.style.backgroundColor = 'white';
+        profile_btn.style.borderRadius = '3px 8px 8px 3px';
+        profile_btn.style.border = '1px solid rgb(132, 192, 233)';
+        profile_btn.style.borderLeftWidth = '0px';
+        profile_btn.style.right = "0.25vw";
+    }
+
+    diary_btn.onclick = function() {
+        home_btn.style.color = 'white';
+        home_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        home_btn.style.borderRadius = '5px';
+        home_btn.style.borderWidth = '0px';
+        home_btn.style.right = '0.15vw';
+
+        profile_btn.style.color = 'white';
+        profile_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        profile_btn.style.borderRadius = '5px';
+        profile_btn.style.borderWidth = '0px';
+        profile_btn.style.right = '0.15vw';
+
+        gallery_btn.style.color = 'white';
+        gallery_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        gallery_btn.style.borderRadius = '5px';
+        gallery_btn.style.borderWidth = '0px';
+        gallery_btn.style.right = '0.15vw';
+
+        adm_btn.style.color = 'white';
+        adm_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        adm_btn.style.borderRadius = '5px';
+        adm_btn.style.borderWidth = '0px';
+        adm_btn.style.right = '0.15vw';
+
+
+        diary_btn.style.color = 'black';
+        diary_btn.style.backgroundColor = 'white';
+        diary_btn.style.borderRadius = '3px 8px 8px 3px';
+        diary_btn.style.border = '1px solid rgb(132, 192, 233)';
+        diary_btn.style.borderLeftWidth = '0px';
+        diary_btn.style.right = "0.25vw";
+    }
+
+    gallery_btn.onclick = function() {
+        home_btn.style.color = 'white';
+        home_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        home_btn.style.borderRadius = '5px';
+        home_btn.style.borderWidth = '0px';
+        home_btn.style.right = '0.15vw';
+
+        profile_btn.style.color = 'white';
+        profile_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        profile_btn.style.borderRadius = '5px';
+        profile_btn.style.borderWidth = '0px';
+        profile_btn.style.right = '0.15vw';
+
+        diary_btn.style.color = 'white';
+        diary_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        diary_btn.style.borderRadius = '5px';
+        diary_btn.style.borderWidth = '0px';
+        diary_btn.style.right = '0.15vw';
+
+        adm_btn.style.color = 'white';
+        adm_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        adm_btn.style.borderRadius = '5px';
+        adm_btn.style.borderWidth = '0px';
+        adm_btn.style.right = '0.15vw';
+
+        gallery_btn.style.color = 'black';
+        gallery_btn.style.backgroundColor = 'white';
+        gallery_btn.style.borderRadius = '3px 8px 8px 3px';
+        gallery_btn.style.border = '1px solid rgb(132, 192, 233)';
+        gallery_btn.style.borderLeftWidth = '0px';
+        gallery_btn.style.right = "0.25vw";
+    }
+
+    adm_btn.onclick = function() {
+        home_btn.style.color = 'white';
+        home_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        home_btn.style.borderRadius = '5px';
+        home_btn.style.borderWidth = '0px';
+        home_btn.style.right = '0.15vw';
+
+        profile_btn.style.color = 'white';
+        profile_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        profile_btn.style.borderRadius = '5px';
+        profile_btn.style.borderWidth = '0px';
+        profile_btn.style.right = '0.15vw';
+
+        diary_btn.style.color = 'white';
+        diary_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        diary_btn.style.borderRadius = '5px';
+        diary_btn.style.borderWidth = '0px';
+        diary_btn.style.right = '0.15vw';
+
+        gallery_btn.style.color = 'white';
+        gallery_btn.style.backgroundColor = 'rgb(132, 192, 233)';
+        gallery_btn.style.borderRadius = '5px';
+        gallery_btn.style.borderWidth = '0px';
+        gallery_btn.style.right = '0.15vw';
+
+        adm_btn.style.color = 'black';
+        adm_btn.style.backgroundColor = 'white';
+        adm_btn.style.borderRadius = '3px 8px 8px 3px';
+        adm_btn.style.border = '1px solid rgb(132, 192, 233)';
+        adm_btn.style.borderLeftWidth = '0px';
+        adm_btn.style.right = "0.25vw";
+    }
+}
